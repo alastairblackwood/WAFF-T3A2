937fe3f02fefa7b511a9f36d48595dc813d2aed7
alastairblackwood <agrizzlybear@icloud.com>
2022-01-17 15:17:39 +1000

    modified jest tests folder

--- test/filterByTerm.js
+++
@@ -1,9 +0,0 @@
-function filterByTerm(inputArr, searchTerm) {
-  if (!searchTerm) throw Error('searchTerm cannot be empty');
-  const regex = new RegExp(searchTerm, 'i');
-  return inputArr.filter(function (arrayElement) {
-    return arrayElement.url.match(regex);
-  });
-}
-
-module.exports = filterByTerm;
--- test/testData.js
+++
@@ -1,6 +0,0 @@
-exports.fakeUserData = {
-  name: 'firstName',
-  email: 'dummy@user.com',
-  password: '********',
-  role: 'user',
-};
